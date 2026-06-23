b0d150f115e3e02ba27a003640934f530e9ccd91
ArtemBartender <Bilenckotema10@gmail.com>
2026-06-23 15:58:53 +0200

    Add Firebase test script to retrieve user data

---
+++ src/testFirebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCa7HuraZ2O0p_Nhta2mMAx4GNGHrxd-eI",
+  authDomain: "rappo-8ed6a.firebaseapp.com",
+  projectId: "rappo-8ed6a",
+  storageBucket: "rappo-8ed6a.firebasestorage.app",
+  messagingSenderId: "880888014436",
+  appId: "1:880888014436:web:5f60cda595d8b84bd56171"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function test() {
+  try {
+     const asd = await getDocs(collection(db, "users"));
+     console.log("users size:", asd.size);
+     asd.docs.slice(0, 5).forEach(doc => console.log(doc.id, doc.data()));
+  } catch(e) {
+     console.error(e);
+  }
+}
+test();
