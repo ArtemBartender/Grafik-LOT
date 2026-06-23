41835b6008b5539eff3cb0d0658a6cd947606c0c
ArtemBartender <Bilenckotema10@gmail.com>
2026-06-23 16:02:17 +0200

    Create fetchRootText.ts

---
+++ src/fetchRootText.ts
@@ -0,0 +1,7 @@
+async function fetchCollections() {
+  const url = "https://firestore.googleapis.com/v1/projects/rappo-8ed6a/databases/(default)/documents";
+  const response = await fetch(url);
+  const data = await response.text();
+  console.log(data);
+}
+fetchCollections();
